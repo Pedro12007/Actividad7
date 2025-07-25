a67411affcedb976a05a57d91769ab9a0119e887
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-07-24 22:56:10 -0600

    Funciones para area y perímetro de rectángulo, y función para verificar números primos

--- main.py
+++ main.py
@@ -18,3 +18,17 @@ def addition(n=1):
 
     return sum, positive, negative, multiple_of_3
 
+def rectangle_area(base=1, height=1):
+    return base * height
+
+def rectangle_perimeter(base=0, height=0):
+    return 2*base + 2*height
+
+def prime_number(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+
