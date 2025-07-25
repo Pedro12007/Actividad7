b27f7ba203bc1d199a47d35ec857bb60c9aea27e
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-07-24 23:13:47 -0600

    Promedio de notas

--- main.py
+++ main.py
@@ -24,11 +24,25 @@ def rectangle_area(base=1, height=1):
 def rectangle_perimeter(base=0, height=0):
     return 2*base + 2*height
 
-def prime_number(num):
+def prime_number(num=1):
     for i in range(2, num):
         if num % i == 0:
             return False
     return True
+
+def grades_average(n=1):
+    sum = 0
+    over_85 = 0
+    risk_zone = 0
+    for i in range(n):
+        grade = int(input('Ingrese las nota: '))
+        sum += grade
+        if grade >= 85:
+            over_85 += 1
+        elif grade < 60:
+            risk_zone += 1
+
+    return sum / n, over_85, risk_zone
 
 
 
