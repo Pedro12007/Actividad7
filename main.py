6ea777fd15209ff0f49a64309f0ca7bb6c1f1ab4
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-07-24 23:37:44 -0600

    Menú de opciones

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from click import option
-
-
 def average(sum=1, length=1):
     return sum / length
 
@@ -8,6 +5,7 @@ def addition(n=1):
     sum = 0
     positive = 0
     negative = 0
+    zeros = 0
     multiple_of_3 = 0
     for i in range(n):
         number = float(input(f'Ingrese el número ({i+1}): '))
@@ -16,10 +14,12 @@ def addition(n=1):
             positive += 1
         elif number < 0:
             negative += 1
+        else:
+            zeros += 0
         if number % 3 == 0:
             multiple_of_3 += 1
 
-    return sum, positive, negative, multiple_of_3
+    return sum, positive, negative, zeros, multiple_of_3
 
 def rectangle_area(base=1, height=1):
     return base * height
@@ -75,3 +75,42 @@ def operations(option, num1, num2):
         case _:
             return 'Opción invalida'
 
+while True:
+    print('Opciones:')
+    print('1. Sumar, obtener promedio y cuántos son positivos, negativos, ceros y múltiplos de 3.')
+    print('2. Calcular área y perímetro de un rectángulo.')
+    print('3. Verificar si un número es primo.')
+    print('4. Calcular promedio de n calificaciones, cuantas son mayores a 85 y cuantas menores a 60.')
+    print('5. Ingresar n números y mostrar el mayor, el menor y cuántos se repiten.')
+    print('6. Calculadora de operaciones básicas (suma, resta, multiplicación y división).')
+    print('7. Salir del programa.')
+    print()
+
+    option = input('Ingrese la opción que desea: ')
+
+    match option:
+        case '1':
+            pass
+
+        case '2':
+            pass
+
+        case '3':
+            pass
+
+        case '4':
+            pass
+
+        case '5':
+            pass
+
+        case '6':
+            pass
+
+        case '7':
+            print('Saliendo del programa...')
+            break
+
+        case _:
+            print('Opción inválida. Intente nuevamente.')
+            print()
